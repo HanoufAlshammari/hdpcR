1345af5dc000e0be628d468745d4ded8c594b8a7
HanoufAlshammari <66770618+HanoufAlshammari@users.noreply.github.com>
2021-05-06 01:43:30 +0300

    Update views.py

--- TheUI/views.py
+++ TheUI/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def home(request):
-    return render(request,'home.html')
-def Home2(request):
-    return render(request,'Home2.html')
+def Hanouf(request):
+    return render(request,'Hanouf.html')
+def Contact(request):
+    return render(request,'Contact.html')
