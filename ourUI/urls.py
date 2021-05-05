b1485adb8b5baa7e4ab663eb61f251f69c3bea01
HanoufAlshammari <66770618+HanoufAlshammari@users.noreply.github.com>
2021-05-06 01:45:10 +0300

    Update urls.py

--- ourUI/urls.py
+++ ourUI/urls.py
@@ -21,6 +21,6 @@ from TheUI import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
-    path('',views.home, name='home'),
-    path('Home2',views.Home2, name='Home2'),
+    path('',views.Hanouf, name='Hanouf'),
+    path('Contact',views.Contact, name='Contact'),
 ]
